ec4a2ede8f68570af5543a77ba16094259bdb231
Dawnlyx <liuyixin2017@163.com>
2017-12-07 21:45:00 +0800

    绘制Fig7

--- Generate_data.py
+++ Generate_data.py
@@ -2,18 +2,10 @@ import numpy as np
 import numpy.random as random
 import matplotlib.pyplot as plt
 
-random.seed(42)
+random.seed(10)
 
-# 产生2x5的标准正态分布样本
 a=random.normal(5,1,(100,1))
 b=random.normal(-5,1,(100,1))
 
-plt.plot(a,b,"r.")
-plt.plot(a,-b,"b.")
-plt.plot(b,a,"g.")
-plt.plot(b,-a,"y.")
-#for x,y in a:
-    #plt.plot(x,y,"r.")
-#for x,y in b:
-    #plt.plot(x,y,"b.")
+plt.plot(a,b,"c.",b,a,"c.",a,-b,"r.",b,-a,"r.")
 plt.show()
