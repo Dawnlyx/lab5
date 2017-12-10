c4b4db42aec5071e169b63f06b832c8a996e846d
Dawnlyx <liuyixin2017@163.com>
2017-12-10 18:50:41 +0800

    生成fig7，并输出.txt

--- Generate_circle_data.py
+++ Generate_circle_data.py
@@ -21,16 +21,31 @@ x1_data=np.zeros(n)
 y1_data=np.zeros(n)
 
 for i,j in enumerate(x):
-    x_data[i]=j+ np.random.normal(scale=0.5, size=1)#x_data[i] = j + np.random.uniform(0,0.5)
+    x_data[i]=j+ np.random.normal(scale=0.3, size=1)#x_data[i] = j + np.random.uniform(0,0.5)
 for i,j in enumerate(y):
-    y_data[i]=j+ np.random.normal(scale=0.5, size=1)
+    y_data[i]=j+ np.random.normal(scale=0.3, size=1)
 for i,j in enumerate(x1):
-    x1_data[i]=j+ np.random.normal(scale=0.5, size=1)
+    x1_data[i]=j+ np.random.normal(scale=0.3, size=1)
 for i,j in enumerate(y1):
-    y1_data[i]=j+ np.random.normal(scale=0.5, size=1)
+    y1_data[i]=j+ np.random.normal(scale=0.3, size=1)
 
+data1 = np.hstack((x_data, y_data))
+data2 = np.hstack((x1_data, y1_data))
+label1 = np.ones((len(data1), 1))
+label2 = np.zeros((len(data2), 1))
+data = np.vstack((data1, data2))
+label = np.vstack((label1, label2))
+dataset=np.hstack((label,data))
+print(dataset)
+
+with open("dataset2.txt",'w') as f:
+    for data in dataset:
+        newline=str(data[0])+'\t'+'1:'+str(data[1])+'\t'+'2:'+str(data[2])+'\n'
+        f.write(newline)
+
+print("x_data=",x_data)
 plt.scatter(x_data, y_data,s=10,c="c")
 plt.scatter(x1_data, y1_data,s=10,c="orange")
-fig.savefig('result1.png')
+fig.savefig('circle.png')
 axes.axis('equal')#绘制出的坐标系长宽不会压缩
 plt.show()
--- Generate_data.py
+++ Generate_data.py
@@ -7,5 +7,25 @@ random.seed(10)
 a=random.normal(5,1,(100,1))
 b=random.normal(-5,1,(100,1))
 
+data1 = np.hstack((a, b))
+data2 = np.hstack((b, a))
+data3 = np.hstack((a, -b))
+data4 = np.hstack((b,-a))
+label1 = np.ones((200, 1))
+label2 = np.zeros((200, 1))
+data = np.vstack((data1, data2, data3, data4))
+label = np.vstack((label1, label2))
+dataset=np.hstack((label,data))
+random.shuffle(dataset)
+print(len(dataset))
+
+with open("dataset1.txt",'w') as f:
+    for data in dataset:
+        newline=str(data[0])+'\t'+'1:'+str(data[1])+'\t'+'2:'+str(data[2])+'\n'
+        f.write(newline)
+
+
+plt.figure("fig7 ")
 plt.plot(a,b,"c.",b,a,"c.",a,-b,"r.",b,-a,"r.")
+plt.savefig('fig7.png')
 plt.show()
